f50d017a7bf9ad614ba7951e2bc40c46242bb090
Thinleyzangmo <12190090.gcit@rub.edu.bt>
2021-03-29 11:32:09 +0600

    lab works

---
+++ TODO_7/app/src/main/java/edu/gcit/todo_7/MainActivity.java
@@ -0,0 +1,36 @@
+package edu.gcit.todo_7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    int i=-1,j;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.text_show_count);
+        if(savedInstanceState !=null){
+            i = savedInstanceState.getInt("count");
+            mShowCount.setText(toString().valueOf(i));
+        }
+    }
+
+    public void CountUp(View view) {
+        i++;
+        mShowCount.setText(String.valueOf(i));
+        j=i;
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle bundle){
+        super.onSaveInstanceState(bundle);
+        bundle.putInt("count",j);
+    }
+
+}
