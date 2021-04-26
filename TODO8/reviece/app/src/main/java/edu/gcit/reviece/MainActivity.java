f6a4702280e4ccd3c1006101cde87eb02532075a
Thinleyzangmo <12190090.gcit@rub.edu.bt>
2021-04-26 21:28:51 +0600

    lab works

---
+++ TODO8/reviece/app/src/main/java/edu/gcit/reviece/MainActivity.java
@@ -0,0 +1,27 @@
+package edu.gcit.reviece;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tview = findViewById(R.id.textview1);
+
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+        if (uri!=null){
+            String msg = uri.toString();
+            tview.setText(msg);
+        }
+
+    }
+}
