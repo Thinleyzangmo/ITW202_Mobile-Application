1fe5cbea444a1e7cd084e40660600fc8b73e02fc
Thinleyzangmo <12190090.gcit@rub.edu.bt>
2021-05-07 21:58:48 +0600

    lab works

--- TODO13/TODO12/app/src/main/java/edu/gcit/todo12/MainActivity.java
+++ TODO13/TODO12/app/src/main/java/edu/gcit/todo12/MainActivity.java
@@ -1,28 +1,47 @@
 package edu.gcit.todo12;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-
 public class MainActivity extends AppCompatActivity {
-
-    public static final String EXTRA_MESSAGE ="com.example.android.droidcafe.extra.MESSAGE";
-
+    public static final String EXTRA_MESSAGE = "gcit.todo_4.MESSAGE";
     String mOrderMessage;
-
+    View mbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mbutton=findViewById(R.id.floatingActionButton);
+        Toolbar tool=findViewById(R.id.toolbar);
+        setSupportActionBar(tool);
+        }
+
+
+    public void donut(View view) {
+        mOrderMessage = getString(R.string.donut_order_message);
+        displayToast(mOrderMessage);
+    }
+
+    public void ice(View view) {
+        mOrderMessage= getString(R.string.ice_cream_order_message);
+        displayToast(mOrderMessage);
+    }
+
+    public void froyo(View view) {
+        mOrderMessage=getString(R.string.froyo_order_message);
+        displayToast(mOrderMessage);
     }
     public void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message,
@@ -32,25 +51,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, order.class);
         intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
         startActivity(intent);
-    }
-
-    public void donutpop(View view) {
-       mOrderMessage = getString(R.string.donut_order_message);
-        displayToast(mOrderMessage);
-
-    }
-
-    public void icepop(View view) {
-        mOrderMessage = getString(R.string.ice_cream_order_message);
-        displayToast(mOrderMessage);
-
-    }
-
-
-    public void froyopop(View view) {
-        mOrderMessage = getString(R.string.froyo_order_message);
-        displayToast(mOrderMessage);
-
-
     }
 }
